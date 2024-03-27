e54184b8fe5427ce8f5ae699dbc62431c6540619
darshan45672 <drshnbhandary@gmail.com>
2024-03-27 09:51:55 +0530

    updated header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     @section('hero')
     <div class="w-full text-center py-32">
         <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-            Welcome to <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
+            Welcome to <span class="text-yellow-500">&lt;Laravel Jet Fila&gt;</span> <span class="text-gray-900"> News</span>
         </h1>
         <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
         <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
@@ -10,7 +10,7 @@
             Reading</a>
     </div>
     @endsection
-    <div class="mb-10">
+    <div class="mb-10 w-full">
         <div class="mb-16">
             <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
             <div class="w-full">
--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
     <div id="header-left" class="flex items-center">
         <div class="text-gray-800 font-semibold">
-            <span class="text-yellow-500 text-xl">&lt;YELO&gt;</span> Code
+            <span class="text-yellow-500 text-xl">&lt;Laravel Jet Fila&gt;</span> Blog
         </div>
         <div class="top-menu ml-10">
             <ul class="flex space-x-4">
