c89fab6f810bd1639edd1217f9c8767ba0873d8f
darshan45672 <drshnbhandary@gmail.com>
2024-03-28 07:21:19 +0530

    created home controller and post controller also defined the relationship

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,6 +12,9 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('home');
+        return view('home',[
+            'featuredPosts' => Post::published()->featured()->latest('published_at')->take(3)->get(),
+            'latestPosts' => Post::published()->latest('published_at')->take(9)->get(),
+        ]);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -17,4 +19,27 @@ class Post extends Model
         'published_at',
         'featured',
     ];
+
+    protected $casts = [
+            'published_at' => 'datetime',
+        ];
+
+    public function author(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function scopePublished($query)
+    {
+        return $query->where('published_at', '<=', Carbon::now());
+    }
+    public function scopeFeatured($query)
+    {
+        return $query->where('featured', true);
+    }
+
+    public function getExcerpt(){
+        return Str::limit(strip_tags($this->body),5);
+    }
+    public function getReadingTime(){
+        return round(str_word_count($this->body)/2);
+    }
 }
